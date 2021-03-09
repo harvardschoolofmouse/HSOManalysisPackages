010ad31baaeb83f8a217b2f65d0a152e90b6c323
harvardschoolofmouse <ahamilos@g.harvard.edu>
2021-03-08 21:05:44 -1000

    Update auto_pull_matlabdatasets.jl

--- JuliaLanguage_Hierarchical_Models/function_library/auto_pull_matlabdatasets.jl
+++ JuliaLanguage_Hierarchical_Models/function_library/auto_pull_matlabdatasets.jl
@@ -2920,4 +2920,71 @@ function SHUFFLE_DAHxmodel_logit_200hx_pkg(path; sessionID ="", getpackagename=f
 	result = results
 	return result#, ndf
 end
+function SHUFFLE_DAonly_logit_200hx_pkg(path; sessionID ="", getpackagename=false, runID=0, suppressFigures=false)
+	#
+	#. This model runs just one model type and returns the df and model data so we can sim data from it.
+	#
+# name the package and runID
+	packagename = join(["SHUFF_DAonly_l_200hx",runID])
+	history_spacing_s = 0.2
+	if getpackagename
+		return packagename
+	end
+	modelNames = [
+	    "SHUFFLE_DAonly",
+	]
+	yID = :LickState
+	predictors = [:Yshuffle,
+					]
+	formulas = [
+	  	@formula(LickState ~ Yshuffle),
+		]
+
+	if history_spacing_s == 0.0
+		error("Improper history_spacing_s! Check the calling fxn, it should specify nonzero")
+	end
+# Try to enter the results folder
+	savepath = joinpath(path, join(["results_", packagename]))
+	figurePath = joinpath(path, join(["figures_", packagename]))
+	try 
+		cd(savepath)
+	catch
+		mkdir(savepath)
+		mkdir(figurePath)
+		cd(savepath)
+	end
+	# do the business of the package on this session
+	#
+	# first, we extract the relevant data: the singletrial, baseline and LOI sets and make a df
+	# (expecting singletrial, baseline, and LOI folders for each dataset with CSV files from matlab)
+	#
+	ndf = extract_data_with_baselineandLOI(path; normalize=true, useHx=true, history_spacing_s=history_spacing_s, omit_cue=false)
+	#
+	# Add shuffle rows to the dataframe
+	#
+	ndf[:Hx10Shuffle] = Random.shuffle(ndf[:Hx10])
+	ndf[:Hx9Shuffle] = Random.shuffle(ndf[:Hx9])
+	ndf[:Hx8Shuffle] = Random.shuffle(ndf[:Hx8])
+	ndf[:Hx7Shuffle] = Random.shuffle(ndf[:Hx7])
+	ndf[:Hx6Shuffle] = Random.shuffle(ndf[:Hx6])
+	ndf[:Hx5Shuffle] = Random.shuffle(ndf[:Hx5])
+	ndf[:Hx4Shuffle] = Random.shuffle(ndf[:Hx4])
+	ndf[:Hx3Shuffle] = Random.shuffle(ndf[:Hx3])
+	ndf[:Hx2Shuffle] = Random.shuffle(ndf[:Hx2])
+
+	
+	results = modelSelectionByAICBICxval(ndf, yID, formulas, modelNames, "logit"; 
+    		n_iters=100,updownsampleYID=true, figurePath=figurePath, savePath = savepath, suppressFigures=suppressFigures)
+# Save each variable to our results folder
+	# Now let's gather up the coefficients of our model and the formulas
+	println(typeof(results))
+	modelData = DataFrame(yID = [yID], predictors=[predictors], th_means=[results.th_summary[1].composite_th], df=[ndf])	
+	modelData_result = [DataFrame() for _=1:nrow(results)]
+	modelData_result[1] = modelData
+	results[:modelData] = modelData_result
+
+# make a working result df with all the results to keep in workspace
+	result = results
+	return result#, ndf
+end
 
